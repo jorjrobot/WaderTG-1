296c37838198882f509e24746e1e32e716df1499
TeleGold <omid.http@gmail.com>
2016-07-09 16:12:25 +0430

    فیلتر کردن کلمات

--- plugins/badword.lua
+++ plugins/badword.lua
@@ -1,15 +1,7 @@
---[[
-
-#
-#     @WaderTGTeam
-#   @WaderTG
-#      
-
-]]
 local function addword(msg, name)
     local hash = 'chat:'..msg.to.id..':badword'
     redis:hset(hash, name, 'newword')
-    return "added the badword:\n\n>"..name
+    return "کلمه: "..name.." فیلتر شد."
 end
 
 local function get_variables_hash(msg)
@@ -23,9 +15,9 @@ local function list_variablesbad(msg)
 
   if hash then
     local names = redis:hkeys(hash)
-    local text = 'badword list:\n\n'
+    local text = 'لیست فیلتر ها:\n\n'
     for i=1, #names do
-      text = text..'> '..names[i]..'\n'
+      text = text..'- '..names[i]..'\n'
     end
     return text
 	else
@@ -34,10 +26,10 @@ local function list_variablesbad(msg)
 end
 
 function clear_commandbad(msg, var_name)
-  --Save on redis  
+
   local hash = get_variables_hash(msg)
   redis:del(hash, var_name)
-  return 'cleared'
+  return 'حذف شد.'
 end
 
 local function list_variables2(msg, value)
@@ -56,7 +48,7 @@ local function list_variables2(msg, value)
 	end
 return 
 end
-      --text = text..names[i]..'\n'
+
     end
   end
 end
@@ -75,26 +67,26 @@ function clear_commandsbad(msg, cmd_name)
   --Save on redis  
   local hash = get_variables_hash(msg)
   redis:hdel(hash, cmd_name)
-  return ''..cmd_name..' clear'
+  return 'کلمه '..cmd_name..' از بین فیلتر ها حذف شد.'
 end
 
 local function run(msg, matches)
-  if matches[2] == 'addword' then
+  if matches[2] == 'فیلتر' then
   if not is_momod(msg) then
-   return 'only for moderators'
+   return 'دست نزن فقط مدیران :|'
   end
   local name = string.sub(matches[3], 1, 50)
 
   local text = addword(msg, name)
   return text
   end
-  if matches[2] == 'badwords' then
+  if matches[2] == 'لیست فیلتر' then
   return list_variablesbad(msg)
-  elseif matches[2] == 'clearbadwords' then
+  elseif matches[2] == 'حذف لیست فیلتر' then
 if not is_momod(msg) then return '_|_' end
   local asd = '1'
     return clear_commandbad(msg, asd)
-  elseif matches[2] == 'remword' or matches[2] == 'rw' then
+  elseif matches[2] == 'پاک کردن فیلتر' or matches[2] == 'حذف فیلتر' then
    if not is_momod(msg) then return '_|_' end
     return clear_commandsbad(msg, matches[3])
   else
@@ -106,11 +98,11 @@ end
 
 return {
   patterns = {
-  "^([!/#])(rw) (.*)$",
-  "^([!/#])(addword) (.*)$",
-   "^([!/#])(remword) (.*)$",
-    "^([!/#])(badwords)$",
-    "^([!/#])(clearbadwords)$",
+  "^(حذف فیلتر) (.*)$",
+  "^(فیلتر) (.*)$",
+   "^(پاک کردن فیلتر) (.*)$",
+    "^(لیست فیلتر)$",
+    "^(حذف لیست فیلتر)$",
 "^(.+)$",
 	   
   },
